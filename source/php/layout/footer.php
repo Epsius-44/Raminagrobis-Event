f720222030de6ccb4ac5ff2fb2b035a6e7f7387f
Valdorion <valdorn2@gmail.com>
2021-12-07 14:14:04 +0100

    footer et cgu

--- source/php/layout/footer.php
+++ source/php/layout/footer.php
@@ -1,2 +1,56 @@
+<footer id="footer">
+    <div class="container">
+        <div class="footer-social-links">
+                <span class="footer-subtitle">
+                    Liens externes
+                </span>
+            <nav class="footer-navbar">
+                <ul class="navbar-nav">
+                    <li>
+                        <a class="nav-link" href=""
+                           title="" target="_bank" rel="noopener">
+                        </a>
+                    </li>
+                    <li>
+                        <a class=""
+                           title="" target="_bank" rel="noopener">
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href=""
+                           title="" target="_bank" rel="noopener">
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="footer-information">
+                <span class="footer-subtitle">
+                    Informations
+                </span>
+            <nav class="footer-navbar">
+                <ul class="navbar-navbar">
+                    <li>
+                        <a class="nav-link" href="contact.html">
+                            Nous contacter :
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="./assets/files/cgu.pdf" target="_blank">
+                            Conditions générales d'utilisation
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+                <span>
+                    © copyright 2021 | site créé par CHANSON Tom, DUCRUET Louis et KASIMIRCZAK Quentin
+                </span>
+        </div>
+    </div>
+</footer>
 </body>
 </html>
