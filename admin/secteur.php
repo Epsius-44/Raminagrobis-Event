39fac901336b258c5c0b715425359dc8def9ded0
Valdorion <valdorn2@gmail.com>
2022-01-11 13:42:10 +0100

    modification des secteur d'activité

--- admin/secteur.php
+++ admin/secteur.php
@@ -14,46 +14,64 @@ include "../source/php/actions/database-connection.php";
             $requete->execute();//executer la requete
             $lignes=$requete->fetchAll();//réccupérer le résultat sous forme d'un tableau
             ?>
+
+            <!-- Listing des secteurs déjà enregistrés dans la base de donnée -->
             <table class="table table-striped">
-                <tr>
-                    <th>Nom</th>
-                    <th>Action</th>
+                <tr> <!-- nom des colonnes -->
+                    <th>
+                        Nom
+                    </th>
+                    <th>
+                        Action
+                    </th>
                 </tr>
                 <?php
-
+                // création du tableau en fonction du nombre de secteurs enregistrés dans la base de donnée
                 foreach($lignes as $l){
                     ?>
                     <tr>
-                        <td><?php echo $l["name"] ?></td>
-                        <td>
-                            <span id="modify_name" class="fas fa-edit"></span><!-- apparition et disparition au clic de la div modify-->
-                            <div id="modify">
-                                <form action="">
-
+                        <td> <!-- affichage du nom du secteur -->
+                            <?php echo $l["name"] ?>
+                        </td>
+                        <td> <!-- option applicable au secteur enregistrés dans la base de donnée-->
+                            <div class="modify">
+                                <span class="fas fa-edit"></span> <!-- apparition et disparition au clic ddu formulaire-->
+                                <form action="../source/php/actions/modify_name_sector.php">
+                                    <input type="text" placeholder="<?php echo $l["name"] ?>" autofocus required/>
+                                    <input type="submit" value="Modifier"/>
                                 </form>
                             </div>
-                            <span class="fas fa-trash"></span><!-- apparition et disparition au clic de la poubelle et d'un "yes/no"-->
+                            <div class="delete">
+                                <span class="fas fa-trash"></span><!-- apparition au clic de la poubelle-->
+                                <form action="../source/php/actions/delete_sector.php">
+                                    <input type="submit" value="Confirme"/>
+                                </form>
+                                <button>
+                                    Annule <!-- disparition du "confirme/annule -->
+                                </button>
+                            </div>
                         </td>
                     </tr>
+
                     <?php
                 }
-
                 ?>
+
             </table>
-            <a href="ajouterCategorie.php" class="btn btn-success">Ajouter</a>
+            <div class="insert"> <!-- création d'un nouveau secteur dans la base de donnée-->
+                <span>
+                    Ajouter
+                </span>
+                <form action="">
+                    <input type="text" placeholder="Secteur"/> <!-- nommage du secteur -->
+                    <input type="submit" value="Créer"/>
+                </form>
+            </div>
         </div>
     </section>
 </main>
 <script>
-    let modify_name = document.getElementById("modify_name");
-    let modify = document.getElementById("modify");
-    modify_name.addEventListener("click", () => {
-        if(getComputedStyle(modify).display != "none"){
-            modify.style.display = "none";
-        } else {
-            modify.style.display = "block";
-        }
-    })
+    <!-- apparition et disparition -->
 </script>
 <?php
 include "../source/php/layout/footer.php";
