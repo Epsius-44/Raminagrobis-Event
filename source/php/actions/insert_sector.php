95cbf249a9d083fa9798cb314051d3d4ca350854
Valdorion <valdorn2@gmail.com>
2022-01-13 10:53:27 +0100

    fin des formulaires

--- source/php/actions/insert_sector.php
+++ source/php/actions/insert_sector.php
@@ -3,8 +3,8 @@
 include "database-connection.php";
 
 //réccupération du nouveau nom du secteur
-$name=filter_input(INPUT_POST, "name");
+$name = filter_input(INPUT_POST, "name");
 
-$requete=$conn->prepare("INSERT INTO `sector`(`name`) VALUES ({$name})"); //creation de la requête
+$requete = $conn->prepare("INSERT INTO `sector`(`name`) VALUES ('{$name}')"); //creation de la requête
 $requete->execute(); //execution de la requête
 header("location: ../../../admin/sector");//retour à la page
