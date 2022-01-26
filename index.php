b3283af90398f76f836200f1b5b3dceca743f9ce
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-01-26 18:25:34 +0100

    Change index style

--- index.php
+++ index.php
@@ -20,54 +20,79 @@ $result = $result[0];
 $sector = sqlCommand("SELECT sector.id, sector.name FROM form_sector JOIN sector ON form_sector.id_sector = sector.id WHERE form_sector.id_form = :form_id", [":form_id" => $form_id], $conn);
 ?>
 <main>
-    <section id="home-hero">
-        <div class="form-header">
-            <h1>🎫 <?= $result['title'] ?></h1>
+    <section id="home-hero" style="background-color: <?php echo "#".$result['color_primary']; ?>">
+        <div class="form-header" style="background: rgba(0, 0, 0, 0.5) url('./assets/img/<?php echo $result['image']; ?>') center;">
+            <h1>🎫 <?php echo $result['title']; ?></h1>
             <p><?= $result['description'] ?></p>
             <hr>
-            <h2>🏭 <?= $result['organisation'] ?></h2>
+            <h2>Organisé par 🏢 <?php echo $result['organisation']; ?></h2>
         </div>
         <div class="container">
             <div class="container-form">
-                <form action="src/actions/create_form_data.php" id="register" method="POST">
+                <form action="src/actions/create_form_data.php" id="register" method="POST" class="form-section">
+                    <h2 style="text-align: center">S'inscrire à l'évènement</h2>
                     <div class="inline-form">
-                        <label for="civility-fild">🧑 Civilité</label>
-                        <select name="civility-fild" id="civility-fild" required>
-                            <option value="" selected disabled>-----</option>
-                            <option value="0">M</option>
-                            <option value="1">Mme</option>
-                            <option value="2">Autre</option>
-                        </select>
-                        <label for="firstname-field">🧑 Prénom</label>
-                        <input type="text"
-                               name="firstname-field"
-                               id="firstname-field"
-                               placeholder="Camille"
-                               required>
-                        <label for="lastname-field">🧑 Nom</label>
-                        <input type="text"
-                               name="lastname-field"
-                               id="lastname-field"
-                               placeholder="Dupont"
-                               required>
+                        <div class="col-form" style="flex-grow: 1;">
+                            <label for="civility-fild">🧑 Civilité</label>
+                            <select name="civility-fild"
+                                    style="border-bottom: 1px solid <?php echo "#".$result['color_secondary']; ?>"
+                                    id="civility-fild"
+                                    class="form-control"
+                                    required>
+                                <option value="" selected disabled>-----</option>
+                                <option value="0">M</option>
+                                <option value="1">Mme</option>
+                                <option value="2">Autre</option>
+                            </select>
+                        </div>
+                        <div class="col-form" style="flex-grow: 3;">
+                            <label for="firstname-field">🧑 Prénom</label>
+                            <input type="text"
+                                   style="border-bottom: 1px solid <?php echo "#".$result['color_secondary']; ?>"
+                                   name="firstname-field"
+                                   id="firstname-field"
+                                   placeholder="Camille"
+                                   class="form-control"
+                                   required>
+                        </div>
+                        <div class="col-form" style="flex-grow: 3;">
+                            <label for="lastname-field">🧑 Nom</label>
+                            <input type="text"
+                                   style="border-bottom: 1px solid <?php echo "#".$result['color_secondary']; ?>"
+                                   name="lastname-field"
+                                   id="lastname-field"
+                                   placeholder="Dupont"
+                                   class="form-control"
+                                   required>
+                        </div>
                     </div>
-                    <label for="email-field">✉ E-mail</label>
+                    <label for="email-field">📧 E-mail</label>
                     <input type="email"
+                           style="border-bottom: 1px solid <?php echo "#".$result['color_secondary']; ?>"
                            name="email-field"
                            id="email-field"
                            placeholder="camille@dupont.fr"
+                           class="form-control"
                            required>
                     <div class="inline-form">
-                        <label for="mobile-field">📞 Téléphone Mobile</label>
-                        <input type="tel"
-                               name="mobile-field"
-                               id="mobile-field"
-                               placeholder="0600000000">
-                        <label for="fixe-field">📞 Téléphone Fixe</label>
-                        <input type="tel"
-                               name="fixe-field"
-                               id="fixe-field"
-                               placeholder="0200000000">
+                        <div class="col-form" style="flex-grow: 1;">
+                            <label for="mobile-field">📞 Téléphone Mobile</label>
+                            <input type="tel"
+                                   style="border-bottom: 1px solid <?php echo "#".$result['color_secondary']; ?>"
+                                   name="mobile-field"
+                                   id="mobile-field"
+                                   placeholder="0600000000"
+                                   class="form-control">
+                        </div>
+                        <div class="col-form" style="flex-grow: 1;">
+                            <label for="fixe-field">📞 Téléphone Fixe</label>
+                            <input type="tel"
+                                   style="border-bottom: 1px solid <?php echo "#".$result['color_secondary']; ?>"
+                                   name="fixe-field"
+                                   id="fixe-field"
+                                   placeholder="0200000000"
+                                   class="form-control">
+                        </div>
                     </div>
                     <label for="peopleType-field">🏭 Entreprise ?</label>
                     <input type="checkbox"
@@ -75,36 +100,54 @@ $sector = sqlCommand("SELECT sector.id, sector.name FROM form_sector JOIN sector
                            id="peopleType-field"
                            onchange="peopleType()">
                     <div class="inline-form">
-                        <label for="sector-field" id="sector-label" hidden>🎯 Secteur</label>
-                        <select name="sector-field" id="sector-field" hidden>
-                            <option value="" selected disabled>Choisir un secteur d'activité</option>
-                            <?php
+                        <div class="col-form" style="flex-grow: 1;">
+                            <label for="sector-field" id="sector-label" class="hidden" hidden>🎯 Secteur</label>
+                            <select name="sector-field"
+                                    style="border-bottom: 1px solid <?php echo "#".$result['color_secondary']; ?>"
+                                    id="sector-field"
+                                    hidden>
+                                <option value="" selected disabled>Choisir un secteur d'activité</option>
+                                <?php
                                 foreach ($sector as $s){
                                     ?>
                                     <option value="<?= $s['id'] ?>"><?= $s['name'] ?></option>
                                     <?php
                                 }
-                            ?>
-                        </select>
-                        <label for="compagny-field" id="compagny-label" hidden>🏭 Entreprise</label>
-                        <input type="text"
-                               name="compagny-field"
-                               id="compagny-field"
-                               placeholder="Raminagrobis"
-                               hidden>
+                                ?>
+                            </select>
+                        </div>
+                        <div class="col-form" style="flex-grow: 2;">
+                            <label for="compagny-field" id="compagny-label" class="hidden" hidden>🏭 Entreprise</label>
+                            <input type="text"
+                                   style="border-bottom: 1px solid <?php echo "#".$result['color_secondary']; ?>"
+                                   name="compagny-field"
+                                   id="compagny-field"
+                                   placeholder="Raminagrobis"
+                                   hidden>
+                        </div>
+
                     </div>
                     <div class="inline-form">
-                        <label for="rgpd-field">RGPD ?</label>
-                        <input type="checkbox"
-                               name="rgpd-field"
-                               id="rgpd-field"
-                               required>
-                        <label for="news-field">Newsletter ?</label>
-                        <input type="checkbox"
-                               name="news-field"
-                               id="news-field">
+                        <div class="col-form" style="flex-grow: 1;">
+                            <label for="rgpd-field">RGPD ?</label>
+                            <input type="checkbox"
+                                   name="rgpd-field"
+                                   id="rgpd-field"
+                                   required>
+                        </div>
+                        <div class="col-form" style="flex-grow: 1;">
+                            <label for="news-field">Newsletter ?</label>
+                            <input type="checkbox"
+                                   name="news-field"
+                                   id="news-field">
+                        </div>
+
                     </div>
-                    <button type="submit">Valider le formulaire</button>
+                    <div class="button">
+                        <button type="submit" style="background-color: <?php echo "#".$result['color_primary']; ?>; border-color: <?php echo "#".$result['color_secondary']; ?>">
+                            Je m'inscrit à l'évènement
+                        </button>
+                    </div>
                 </form>
             </div>
         </div>
@@ -121,7 +164,9 @@ $sector = sqlCommand("SELECT sector.id, sector.name FROM form_sector JOIN sector
         if (document.getElementById("peopleType-field").checked) {
             for (const field in form_field) {
                 form_field[field].hidden = false;
+                form_field[field].setAttribute("class", "");
                 if (field%2===0) {
+                    form_field[field].setAttribute("class", "form-control");
                     form_field[field].required = true;
                 }
             }
@@ -129,6 +174,7 @@ $sector = sqlCommand("SELECT sector.id, sector.name FROM form_sector JOIN sector
         else{
             for (const field in form_field) {
                 form_field[field].hidden = true;
+                form_field[field].setAttribute("class", "hidden");
                 if (field%2===0) {
                     form_field[field].required = false;
                 }
