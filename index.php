fe20050c3e7e2eafd4bd9d5a0e8f4edb92c6058a
Louis-Ducruet <louis.ducruet@epsi.fr>
2022-02-03 15:50:24 +0100

    Fix css glitch

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $sector = sqlCommand("SELECT sector.id, sector.name FROM form_sector JOIN sector
 ?>
 <main>
     <section id="home-hero" style="background-color: <?php echo "#".$result['color_primary']; ?>">
-        <div class="form-header" style="background: rgba(0, 0, 0, 0.5) url('./assets/img/<?php echo $result['image']; ?>') center;">
+        <div class="form-header" style="background: rgba(0, 0, 0, 0.5) url('./assets/img/<?php echo $result['image']; ?>') center;background-size: cover;">
             <h1>🎫 <?php echo $result['title']; ?></h1>
             <p><?= $result['description'] ?></p>
             <hr>
